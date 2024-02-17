bfa2887512162dffa31caa58761e8125f1f5924a
Freemano <freemano@MACs-MacBook-Pro.local>
2024-02-17 22:46:24 +0300

    adde the online url

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Index from '@/views/Index.vue'
 import Login from '@/views/auth/Login.vue'
 import Register from '@/views/auth/Register.vue'
+import Verify from '@/views/auth/Verify.vue'
+import Admin_Dashboard from '@/views/dashboard/admin/Index.vue'
+import Case_details from '@/views/dashboard/admin/Case_details.vue'
+import Show_admin from '@/views/dashboard/super_admin/Show_admin.vue'
+import View_evidence from "@/views/dashboard/admin/View_evidence.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,6 +23,28 @@ const router = createRouter({
     {
       path: '/register',
       component : Register
+    },
+    {
+      path: '/verify/:id',
+      component : Verify
+    },
+    {
+      path: '/dashboard/admin',
+      component : Admin_Dashboard
+    },
+    {
+      path: '/dashboard/admin/case_details/:case_id/:key_id',
+      component : Case_details
+    }
+    ,
+    {
+      path: '/dashboard/super_admin/show_admin',
+      component : Show_admin
+    }
+    ,
+    {
+      path: '/dashboard/admin/view_evidence/:case_id/:secret_key',
+      component: View_evidence
     }
   ]
 })
