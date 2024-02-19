d3c7413eb890c960682406aa1ba4bd1a5121fdd7
Freemano <freemano@MACs-MacBook-Pro.local>
2024-02-19 17:41:25 +0300

    endpoints

--- src/compossables/auth.js
+++ src/compossables/auth.js
@@ -4,11 +4,11 @@ import axios from "axios";
 export function auth(){
     const authHeader = { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } };
 
-    // const base_url = ref('https://forensic.kwetunyumbani.online/api/')
-    // const storage = ref('https://forensic.kwetunyumbani.online/backend/public/Evidences/')
+    const base_url = ref('https://forensic.kwetunyumbani.online/api/')
+    const storage = ref('https://forensic.kwetunyumbani.online/backend/public/Evidences/')
 
-    const base_url = ref('http://127.0.0.1:8000/api/')
-    const storage = ref('http://127.0.0.1:8000/Evidences/')
+    // const base_url = ref('http://127.0.0.1:8000/api/')
+    // const storage = ref('http://127.0.0.1:8000/Evidences/')
 
     const   authUser = async () => {
         try {
