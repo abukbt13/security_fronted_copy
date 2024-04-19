0f7c83e347aa56a8f8db68f048b6b73307df9c4f
Freemano <freemano@Freeman.local>
2024-04-19 07:04:25 +0300

    finished

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,10 +5,29 @@ import Header from "@/views/includes/Header.vue";
 </script>
 <template>
   <Header />
-<p>About us</p>
+    <div class="container">
+      <h1 class="text-center">About Us</h1>
+      <p class="fs-3">We are dedicated to providing a cutting-edge digital forensic evidence management system that meets the needs of law enforcement, legal professionals, and corporate investigators. Our system is designed to securely store, manage, and analyze digital evidence, ensuring its integrity and authenticity for legal proceedings and investigations.</p>
+
+      <p class="fs-3">Our mission is to empower our clients with the tools they need to manage digital evidence efficiently and securely. We envision a future where digital forensics is streamlined and accessible, helping to bring justice and clarity to complex cases.</p>
+
+    </div>
+    <div class="container">
+      <div class=" d-flex flex-column align-items-center">
+        <h2 class="fs-3 text-primary">Key Features:</h2>
+        <ul>
+          <li>Secure evidence storage with access control and auditing.</li>
+          <li>Advanced encryption to protect sensitive data.</li>
+          <li>User-friendly interface for easy management and analysis.</li>
+          <li>Compliance with legal and regulatory standards.</li>
+        </ul>
+      </div>
+    </div>
 </template>
 
 
 <style scoped>
-
+li{
+  font-size: 31px;
+}
 </style>
